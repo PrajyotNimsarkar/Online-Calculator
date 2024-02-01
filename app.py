e803c881cf60121846f78fba4dd2a3cd0788cd52
Prajyot Nimsarkar <129411961+PrajyotNimsarkar@users.noreply.github.com>
2024-02-01 15:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def calculator():
             else:
                 result = num1 / num2
 
-    return render_template('calculator.html', result=result)
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
